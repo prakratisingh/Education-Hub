f76bd157a732d8ba379a54941d320ba090265f55
Prakrati Singh <500082638@stu.upes.ac.in>
2023-07-31 22:57:41 +0530

    Create basics.php

---
+++ basics.php
@@ -0,0 +1,22 @@
+<?php
+//variables are made using $ sign
+//PHP is NOT a case sensitive language
+$var1 = 31;
+$var2 = 45;
+
+echo $var1;
+echo $var2;
+//to put new line in the page
+echo "<br>";
+echo $var1+$var2;
+
+
+//Operators in PHP
+/*
+- arithmetic operators : 
+- assignment operators : 
+- comparision operators : 
+- increment/decrement operators : 
+- logical operators : 
+*/
+?>
